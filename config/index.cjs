876b11cb2c1b5676817e7612affa23be0333a368
shuaiguozeng <157680150+shuaiguozeng@users.noreply.github.com>
2024-01-25 16:21:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,26 +27,28 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      // horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '生日', name: '姐姐', year: '1998', date: '06-18',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '弟弟', year: '2000', date: '01-03',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相恋', year: '2023', date: '12-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-12-02' },
+        // 相识
+        { keyword: 'meet_day', date: '2023-04-20' },
+        // 初遇
+        {keyword: 'meet2_day', date: '2023-08-24' },
       ],
     },
   ],
